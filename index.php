74be7fa0a63c91fe6c7675856be51b5710dfb627
Weizhong Li <weizhongli1987@gmail.com>
2018-01-26 01:38:14 +0000

    init commit batch 1

---
+++ index.php
@@ -0,0 +1,10 @@
+<?php
+$title = 'Home';
+require("includes/common.php");
+?>
+<div id="maininner">
+<?php include("includes/desc.php"); print $page_overview; ?>
+</div>
+<?php
+require("includes/footer.php");
+?>
